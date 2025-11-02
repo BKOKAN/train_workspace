075aeba9375bf9e1142e54cf69160c16a1f56e04
Božo Kokan <bozokokan11@hotmail.com>
2025-11-02 16:45:56 +0100

    update
    test

---
+++ folder_za_vjezbe/vjezba_nakon_kratke_pauze/script.js
@@ -0,0 +1,15 @@
+const forma = document.querySelector(".forma");
+const usernameInput = document.querySelector("#username");
+const firstNameInput = document.querySelector("#firstName");
+const laseNameInput = document.querySelector("#lastName");
+const emailInput = document.querySelector("#email");
+const registerBtn = document.querySelector(".register");
+const restartBtn = document.querySelector(".restart");
+
+class info {
+  firstName = "";
+  lastName = "";
+  email = "";
+}
+
+registerBtn.addEventListener("click", () => {});
